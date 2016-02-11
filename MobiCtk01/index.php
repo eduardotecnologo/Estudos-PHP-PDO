adf5686582f192e4d9b63ea78487e79d324b988a
eduardotecnologo <eduardotecnologo@hotmail.com>
2016-02-10 23:31:31 -0200

    Criação do Layout do Sistema Imobiliário

---
+++ MobiCtk01/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<title>Sistema | Imobiliário ctk 01</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Login:::Sistema Imobiliário ctk Exemplo 01">
+    <meta name="author" content="Eduardo Alexandre">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" href="imgagens/favicon.ico">
+</head>
+<body>
+<div id="box">
+	<div id="header_topo">
+
+	         <a href="index.php"><img src="imagens/ctkimobi_logo.png" alt="Home" title="Home" border="0" /></a>
+	         		<ul>
+	         			<li><a href="#">Comprar</a></li>
+	         			<li><a href="#">Alugar</a></li>
+	         			<li><a href="#">Anúnciar</a></li>
+	         			<li><a href="#">Fale Conosco</a></li>
+	         			
+	         		</ul>
+	   </div><!--fecha a DIV topo-->
+	<div id="header_navegar">
+	      <div id="header_navegar_central">
+	            <div id="header_navegar_central_anunciante">
+	            	<form name="central_anunciante" action="" method="POST">
+	            		<label>
+	            			<span>Usuário:</span>
+	            				    <input type="text" name="usuario"/>
+	            		</label>
+	            		<label>
+	            			<span>Senha:</span>
+	            				    <input type="password" name="senha" class="senha" />
+	            			</label>
+	            			<input type="submit" name="Enviar" value="Entrar" class="btn"/>
+	            		</form>
+	            		<p>[Esqueci a Senha]</p>
+	            	</div><!--fecha div header_navegar_central_anunciante-->
+	            	<div id="header_navegar_central_anuncie">
+	            		
+	            	</div><!--fecha header_navegar_central_anuncia-->
+	            </div><!--fecha header navegar centyral-->
+	            <div id="header_navegar_filtro">
+	            	
+	            </div><!--fecha header navegar filtro-->
+	            <div id="header_navegar_publicidade">
+	            <h1>Publicidade</h1>
+	            		<a href="#"></a><img src="midias/01.jpg" alt="" title="" /></a>
+	            	
+	            </div><!--fecha header navegar publicidade-->
+	   	
+	   </div><!--fecha a DIV navegar-->
+    <div id="imoveis_home">
+    	
+       </div><!--fecha a DIV imoveis home-->
+    <div id="imoveis_home_lista">
+    	
+       </div><!--fecha a DIV imoveis home lista-->   
+</div><!--fecha a DIV box-->
+    <divn id="footer">
+       </div><!--fecha DIV footer--->
+</body>
+</html>
