bba8fb40672a8b750c4b2b01eedcacd8f4c11a8c
eduardotecnologo <eduardotecnologo@hotmail.com>
2016-07-12 21:29:35 -0300

    Iniciando uma loja virtual

--- lojaVirtual01/login.php
+++ lojaVirtual01/login.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="require/css/login.css"/>
 <form action="javascript:void(0)">
-	<p>Login BigCred Financeira</p>
+	<p>Exemplo de LojaVirtual</p>
 	<div><label for="eLogin">E-mail:</label><input type="text" id="eLogin" ></div>
 	<div><label for="sLogin">Senha:</label><input type="password" id="sLogin" maxlenght="12" ></div>
 	<div><button onclick="fctLogin()">Entrar</button></div>
