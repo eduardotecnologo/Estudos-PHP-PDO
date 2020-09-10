bd64a76f2ef8cfca3f7d991e733e3b54d0de308a
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-09-10 17:50:30 -0300

    Criando habilidades

--- laravelSanctum/database/seeds/DatabaseSeeder.php
+++ laravelSanctum/database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        factory(User::class, 1)->create([
+            "email" => "edu@email.com"
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
